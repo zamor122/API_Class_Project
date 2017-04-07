468443e9851585f91ae41cedb4a45a299e9e1b14
Shayne Zamora <Shayne Zamora>
2017-04-07 00:38:49 -0700

    Adds more functionality and Types.txt

--- Main.py
+++ Main.py
@@ -6,8 +6,6 @@
 
 #imports and needs:
 import googlemaps
-import yelp
-from googlemaps import places
 import requests
 import json, urllib
 import re
@@ -20,13 +18,36 @@ ip = requests.get(get_ip)
 j = json.loads(ip.text)
 lat = j['latitude']
 lng = j['longitude']
+##convert IPs to string
+userLat = str(lat)
+userLng = str(lng)
+#Get search radius from user
+print ('1. Enter a search radius in meters (must be less than 50,000): ')
+print ('WARNING: If a 0 or no number is entered it will give you the results immediately around you')
+searchRadius = raw_input(str())
+
+##try opening file for types if not close
+try:
+    file = open('Types.txt')
+except:
+    print IOError
+##get user desired place type
+print '\n','2. Enter the type of place you are searching for the options are: ', '\n'
+for line in file:
+    print line
+userType = raw_input('')
+
 ###################
 #Find places nearby
-#searchRadius=raw_input('Enter the search radius in meters (must be >50,000: ')
 #create request
-google_request = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&type=restaurant&keyword=cruise&key=AIzaSyAMWWPiiqKIMReF93CjlGf2eaK6K-YMgFI'
+google_request = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location='+userLat+', '+userLng+'&radius='+searchRadius+'&type='+userType+'&keyword=&key=AIzaSyAMWWPiiqKIMReF93CjlGf2eaK6K-YMgFI'
 #store request
 requestResults = urllib.urlopen(google_request)
 #get results in json form
 result = json.load(requestResults)
-print result
+#print result
+#print result in nice format
+for i in range(0,len(result['results'][0]['name'])):
+    listOfItems = result['results'][i]['name']
+    niceList = '\n' + listOfItems
+    print niceList
